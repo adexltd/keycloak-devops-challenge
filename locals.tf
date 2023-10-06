6a04eb2be497a6d80e46806dd882866a7fda5e17
roshanramangiri <roshanramangiri@gmail.com>
2023-10-06 10:00:47 +0545

    DEVX-142-feat/customized_theme_realm removing .env

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 locals {
   secrets = {
-    db_username               = env.db_username 
-    db_password               = env.db_password
-    keycloak_admin_username   = env.keycloak_admin_username
-    keycloak_admin_password   = env.keycloak_admin_password
-    certificate_arn_us-east-1 = env.certificate_arn_us-east-1
-    certificate_arn_us-east-2 = env.certificate_arn_us-east-2
+        db_username               = "keycloak"
+        db_password               = "secrectpassword"
+        keycloak_admin_username   = "admin"
+        keycloak_admin_password   = "secrectpassword"
+        certificate_arn_us-east-1 = "arn:aws:acm:us-east-1:426857564226:certificate/3fc0c3bc-90f6-4600-bcd5-6ffeb59db6db"
+        certificate_arn_us-east-2 = "arn:aws:acm:us-east-2:426857564226:certificate/f53ef742-9dae-4c8e-b76d-1b0ba1cda8c5"
   }
 }
