e5815fb38fd59ed5eeed9aa49c8c069b3d477eb4
roshanramangiri <roshanramangiri@gmail.com>
2023-10-10 20:19:50 +0545

    DEVX-142-feat/customized_theme_realm oidc role changed

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,7 +2,7 @@
 REGION="us-east-2"
 IMAGE_NAME="keycloak"
 MODULE_PATH=$MODULE_PATH
-aws ecr get-login-password --region $REGION --profile $PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker build -t $IMAGE_NAME:latest  $MODULE_PATH/../../../customization/
 docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
